e69b2d4d74b371384fc4f615cd8c3f06918b0965
cyber-chalk <52194915+cyber-chalk@users.noreply.github.com>
2022-10-13 13:55:17 +1100

    Update cookies.js

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -1,13 +1,15 @@
 let settings = document.getElementsByClassName("cookie-container")[0];
 
-const value = ("; " + document.cookie)
+const gettingCookie = ("; " + document.cookie)
 	.split(`; ALREADY_VERIFIED=`)
 	.pop()
 	.split(";")[0];
-if (document.cookie == value) settings.style.display = "none";
+if (document.cookie === "name") { console.log(document.cookies); 
+	settings.style.display = "none"
+};
 
 let close = document.getElementById("close");
-close.addEventListener("click", function () {
+close.addEventListener("click",  function () {
 	console.log("no cookies in use");
 });
 
